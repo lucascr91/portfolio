c14bf6e01d5633e7fc9637f96959a7a920ffa41a
lucascr91 <lucas.ecomg@gmail.com>
2021-08-01 15:24:23 -0300

    add cv and github tags

---
+++ tagueamento.js
@@ -0,0 +1,7 @@
+const downloadCV = () => {
+    ga('send', {'hitType': 'event', 'eventCategory': 'menu', 'eventAction': 'CV', 'eventLabel': 'CV' });
+}
+
+const go2Github = () => {
+    ga('send', {'hitType': 'event', 'eventCategory': 'menu', 'eventAction': 'Github', 'eventLabel': 'Github' });
+}
