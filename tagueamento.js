9a7b2875a51d1649b45d0f9f68d642b28ca8ec40
lucascr91 <lucas.ecomg@gmail.com>
2021-08-01 15:35:34 -0300

    add remain tags

--- tagueamento.js
+++ tagueamento.js
@@ -5,3 +5,15 @@ const downloadCV = () => {
 const go2Github = () => {
     ga('send', {'hitType': 'event', 'eventCategory': 'menu', 'eventAction': 'Github', 'eventLabel': 'Github' });
 }
+
+const go2Lattes = () => {
+    ga('send', {'hitType': 'event', 'eventCategory': 'menu', 'eventAction': 'Lattes', 'eventLabel': 'Lattes' });
+}
+
+const go2So = () => {
+    ga('send', {'hitType': 'event', 'eventCategory': 'menu', 'eventAction': 'Stackoverflow', 'eventLabel': 'Stackoverflow' });
+}
+
+const go2Linkedin = () => {
+    ga('send', {'hitType': 'event', 'eventCategory': 'menu', 'eventAction': 'Linkedin', 'eventLabel': 'Linkedin' });
+}
